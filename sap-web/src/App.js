676b3992b4a42118321c37bd10f0ae698256ec15
joneikel <joneikel2010@gmail.com>
2020-10-14 09:23:28 -0400

    fast update

---
+++ sap-web/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload
+          
+           testing.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React algo mas
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
